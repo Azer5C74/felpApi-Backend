ea04ea0592f3b820cffa62e63efce2104d010165
Azerwht <azer.taboubi@example.com>
2021-05-16 01:10:28 +0100

    added scoreQuery formula for business term

--- controllers/SearchEngine.js
+++ controllers/SearchEngine.js
@@ -24,11 +24,86 @@ exports.searchQuery = asyncHandler(async (req, res, next) => {
 // Index all documents
     miniSearch.addAll(businessList)
     const {term} = req.query
-    let suggestions = miniSearch.autoSuggest(term, { fuzzy: 0.2 })
+    let suggestions = miniSearch.autoSuggest(term, { fuzzy: 0.1 })
+    let results = miniSearch.search(term,{prefix:true, fuzzy:0.1})
 
-    let results = miniSearch.search(term,{prefix:true, fuzzy:0.2})
-// Search with default options. It will return the id of the matching documents,
-// along with a relevance score and match information
+
+    let resultsArray=[]
+    const currentLocation={}
+    currentLocation.altitude=35.335256
+    currentLocation.longitude=10.890310
+
+
+
+
+    if (results){
+        //to perform price sorting, we have to confirm that the entered term is a menu item
+        if(Object.values(results[0].match)=="menu"){
+            console.log(Object.keys(results[0].match))
+            for (let i = 0; i < results.length; i++) {
+
+                let {altitude,longitude}=results[i].location
+                console.log(results[0])
+                d=getDistanceFromLatLonInKm(currentLocation.altitude,currentLocation.longitude,altitude,longitude);
+                //taking price into account
+                scoreQuery=(results[i].averageRating*0.3)-(d*0.3)+(results[i].score)
+                results[i].scoreQuery=scoreQuery;
+                console.log(results[i])
+            }
+
+        }
+
+
+        //we have to confirm that the entered term is a name of a business
+        if(Object.values(results[0].match)=="name"){
+
+        for (let i = 0; i < results.length; i++) {
+
+            let {altitude,longitude}=results[i].location
+            console.log(results[0])
+            d=getDistanceFromLatLonInKm(currentLocation.altitude,currentLocation.longitude,altitude,longitude);
+            scoreQuery=(results[i].averageRating*0.3)-(d*0.3)+(results[i].score)
+            results[i].scoreQuery=scoreQuery;
+            console.log(results[i])
+        }
+        results.sort(((a,b) => (a.scoreQuery < b.scoreQuery) ? 1 : -1 ))
+            console.log(results)
+    }
+    }
+
+
+    // sort using average rating
+    function compareAverageRating( a, b ) {
+        if ( a.averageRating > b.averageRating ){
+            return -1;
+        }
+        if ( a.averageRating < b.averageRating ){
+            return 1;
+        }
+        return 0;
+    }
+
+    //calculate distance in KLM between two locations
+    function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+        var R = 6371; // Radius of the earth in km
+        var dLat = deg2rad(lat2-lat1);  // deg2rad below
+        var dLon = deg2rad(lon2-lon1);
+        var a =
+            Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+            Math.sin(dLon/2) * Math.sin(dLon/2)
+        ;
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c; // Distance in km
+        return d;
+    }
+    function deg2rad(deg) {
+        return deg * (Math.PI/180)
+    }
+
+
+
+
     if(results) {
         res.status(200).json({
             success: true,
