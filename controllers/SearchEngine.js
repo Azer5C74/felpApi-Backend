9ff685f72b4206ed46447a9485be3c075cb8967f
Azerwht <azer.taboubi@example.com>
2021-05-22 17:49:44 +0100

    added more sorting params and coef

--- controllers/SearchEngine.js
+++ controllers/SearchEngine.js
@@ -14,74 +14,63 @@ exports.searchQuery = asyncHandler(async (req, res, next) => {
     if(!businessList)
         return next(new ErrorResponse("Business List is empty", 400));
 
-    // const { longitude, altitude } = req.params;
-    // const business = await Business.findOne({
-    //     "location.longitude": longitude,
-    //     "location.altitude": altitude
-    // });
 
 
 // Index all documents
     miniSearch.addAll(businessList)
+//get the entered term from the query
     const {term} = req.query
     let suggestions = miniSearch.autoSuggest(term, { fuzzy: 0.1 })
     let results = miniSearch.search(term,{prefix:true, fuzzy:0.1})
 
-
-    let resultsArray=[]
     const currentLocation={}
     currentLocation.altitude=35.335256
     currentLocation.longitude=10.890310
 
 
-
-
-    if (results){
+    if (results.length){
         //to perform price sorting, we have to confirm that the entered term is a menu item
         if(Object.values(results[0].match)=="menu"){
-            console.log(Object.keys(results[0].match))
             for (let i = 0; i < results.length; i++) {
 
                 let {altitude,longitude}=results[i].location
-                console.log(results[0])
                 d=getDistanceFromLatLonInKm(currentLocation.altitude,currentLocation.longitude,altitude,longitude);
+                console.log(d*0.3/10)
                 //taking price into account
-                scoreQuery=(results[i].averageRating*0.3)-(d*0.3)+(results[i].score)
+                scoreQuery=(results[i].averageRating*0.2)-(d*0.4)/100+(results[i].score*0.2)
                 results[i].scoreQuery=scoreQuery;
-                console.log(results[i])
+                //console.log(results[i])
             }
-
+            results.sort(((a,b) => (a.scoreQuery < b.scoreQuery) ? 1 : -1 ));
         }
 
 
         //we have to confirm that the entered term is a name of a business
         if(Object.values(results[0].match)=="name"){
 
-        for (let i = 0; i < results.length; i++) {
+            for (let i = 0; i < results.length; i++) {
 
-            let {altitude,longitude}=results[i].location
-            console.log(results[0])
-            d=getDistanceFromLatLonInKm(currentLocation.altitude,currentLocation.longitude,altitude,longitude);
-            scoreQuery=(results[i].averageRating*0.3)-(d*0.3)+(results[i].score)
-            results[i].scoreQuery=scoreQuery;
-            console.log(results[i])
-        }
-        results.sort(((a,b) => (a.scoreQuery < b.scoreQuery) ? 1 : -1 ))
-            console.log(results)
+                let {altitude,longitude}=results[i].location;
+                d=getDistanceFromLatLonInKm(currentLocation.altitude,currentLocation.longitude,altitude,longitude);
+                scoreQuery=(results[i].averageRating*0.3)-(d*0.3)+(results[i].score*0.2)
+                results[i].scoreQuery=scoreQuery;
+            }
+            results.sort(((a,b) => (a.scoreQuery < b.scoreQuery) ? 1 : -1 ));
+            //console.log(results)
     }
     }
 
-
-    // sort using average rating
-    function compareAverageRating( a, b ) {
-        if ( a.averageRating > b.averageRating ){
-            return -1;
-        }
-        if ( a.averageRating < b.averageRating ){
-            return 1;
-        }
-        return 0;
-    }
+    //
+    // // sort using average rating
+    // function compareAverageRating( a, b ) {
+    //     if ( a.averageRating > b.averageRating ){
+    //         return -1;
+    //     }
+    //     if ( a.averageRating < b.averageRating ){
+    //         return 1;
+    //     }
+    //     return 0;
+    // }
 
     //calculate distance in KLM between two locations
     function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
